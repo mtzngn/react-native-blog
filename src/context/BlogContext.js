43dd765a7c435827413677745dfe6faed5f303c0
taha <mt.zngn@gmail.com>
2021-07-09 11:02:05 +0100

    automatic context providing implemented

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,27 +1,19 @@
-import React, { useReducer } from 'react';
-
-const BlogContext = React.createContext();
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch(action.type) {
         case 'add_blogpost':
             return [...state, { title: `Blog Post ${state.length + 1}`}];
         default: 
-        return state;
-    }
-}
-
-
-export const BlogProvider = ({ children }) => {
-    const [blogPosts, dispatch] = useReducer(blogReducer, []);
-    
-    const addBlogPost = () => {
-        dispatch({ type: 'add_blogpost'})
+            return state;
     };
-
-    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }} >
-        {children}
-    </BlogContext.Provider>
 };
 
-export default BlogContext;
+const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost'});
+}; 
+
+export const { Context, Provider} = createDataContext(
+    blogReducer,
+    { addBlogPost },
+    []);
