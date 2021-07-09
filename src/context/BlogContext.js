49342628c8f722a96232d11c9d9a6f5814ac9883
taha <mt.zngn@gmail.com>
2021-07-09 10:43:15 +0100

    managed state with useReducer instead of useState

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch(action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog Post ${state.length + 1}`}];
+        default: 
+        return state;
+    }
+}
+
+
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={5}>
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
+    
+    const addBlogPost = () => {
+        dispatch({ type: 'add_blogpost'})
+    };
+
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }} >
         {children}
     </BlogContext.Provider>
 };
