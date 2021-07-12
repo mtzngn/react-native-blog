bd5d7e513dd68370b06bfacd6282b8d9229299ac
taha <mt.zngn@gmail.com>
2021-07-12 09:31:02 +0100

    edit_blog reducer created

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,12 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch(action.type) {
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id
+                ? action.payload
+                : blogPost;
+            });
         case 'add_blogpost':
             return [...state, { 
                 id: Math.floor(Math.random() * 99999),
@@ -18,7 +24,7 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
     return (title, content, callback) => {
         dispatch({ type: 'add_blogpost', payload: { title, content }});
-        callback()
+        if (callback) callback()
     };
 }; 
 const deleteBlogPost = (dispatch) => {
@@ -27,7 +33,14 @@ const deleteBlogPost = (dispatch) => {
     }
 }
 
+const editBlogPost = (dispatch) => {
+    return (id, title, content, callback) => {
+        dispatch({type: 'edit_blogpost', payload: {id, title, content}});
+        if (callback) callback()
+    };
+};
+
 export const { Context, Provider} = createDataContext(
     blogReducer,
-    { addBlogPost, deleteBlogPost },
+    { addBlogPost, deleteBlogPost, editBlogPost },
     [{title: 'Test Post', content: 'testing', id: 1}]);
